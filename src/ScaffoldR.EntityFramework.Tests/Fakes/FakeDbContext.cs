ccd8c906b955153bcc4bc67d93dddcbdb1aba8f7
Jan Hartmann <kontakt@janhartmann.dk>
2015-11-20 13:38:56 +0100

    Added fake connectionstring for fake db context

--- src/ScaffoldR.EntityFramework.Tests/Fakes/FakeDbContext.cs
+++ src/ScaffoldR.EntityFramework.Tests/Fakes/FakeDbContext.cs
@@ -4,6 +4,10 @@ namespace ScaffoldR.EntityFramework.Tests.Fakes
 {
     public class FakeDbContext : DbContext
     {
+        protected FakeDbContext() : base(@"Server=(local)\SQL2008R2SP2;Database=master;User ID=sa;Password=Password12!")
+        {
+        }
+
         public virtual DbSet<FakeCustomer> FakeCustomers { get; set; }
     }
 }
