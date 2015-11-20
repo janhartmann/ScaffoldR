8fa9c07f81195774b26638ad91986582a6bf4528
Jan Hartmann <kontakt@janhartmann.dk>
2015-11-20 13:49:17 +0100

    Fixed test

--- src/ScaffoldR.EntityFramework.Tests/Fakes/FakeDbContext.cs
+++ src/ScaffoldR.EntityFramework.Tests/Fakes/FakeDbContext.cs
@@ -4,10 +4,6 @@ namespace ScaffoldR.EntityFramework.Tests.Fakes
 {
     public class FakeDbContext : DbContext
     {
-        protected FakeDbContext() : base(@"Server=(local)\SQL2008R2SP2;Database=master;User ID=sa;Password=Password12!")
-        {
-        }
-
         public virtual DbSet<FakeCustomer> FakeCustomers { get; set; }
     }
 }
